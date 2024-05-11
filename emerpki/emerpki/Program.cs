fd8a915ee8d76b3c5cd8285775203e57cd2e214a
Scott Guyton <kiljoy001@gmail.com>
2024-05-11 14:14:41 -0700

    Created application bones, menu and debug switch to testnet

---
+++ emerpki/emerpki/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace emerpki;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        bool isDebug = false;
+        bool emertestnet = false;
+
+        if (args.Length > 0 && args[0] == "--debug")
+        {
+            isDebug = true;
+        }
+
+        if (args.Length > 1 && args[1] == "--testnet")
+        {
+            emertestnet = true;
+        }
+        Menu ProgramMenu = new Menu(isDebug, emertestnet);
+        ProgramMenu.DisplayTestMenu();
+    }
+
+}
+
+
