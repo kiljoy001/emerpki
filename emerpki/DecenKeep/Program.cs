c6226472385ee07e69c685262786e87c64b8999c
Scott Guyton <kiljoy001@gmail.com>
2024-05-25 08:35:21 -0700

    updated application program to reflect removal of emercoin blockchain

--- emerpki/DecenKeep/Program.cs
+++ emerpki/DecenKeep/Program.cs
@@ -5,20 +5,9 @@ class Program
     
     static void Main(string[] args)
     {
-        var isDebug = false;
-        var connectEmercoinTestNet = false;
-
-        if (args.Length > 0 && args[0] == "--debug")
-        {
-            isDebug = true;
-        }
-
-        if (args.Length > 1 && args[1] == "--testnet")
-        {
-            connectEmercoinTestNet = true;
-        }
-        var programMenu = new Menu(isDebug, connectEmercoinTestNet);
-        programMenu.DisplayTestMenuAsync();
+        bool isDebug = args.Length > 0 && args[0] == "--debug";
+        //var programMenu = new Menu(isDebug, connectEmercoinTestNet);
+        //programMenu.DisplayTestMenuAsync();
     }
 
 }
