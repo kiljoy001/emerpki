a6e8e014e0294edd124b5e5094804cb57e0a4ea0
Scott Guyton <kiljoy001@gmail.com>
2024-05-30 21:17:48 -0700

    added project files

--- emerpki/DecenKeep/obj/Debug/net8.0/DecenKeep.AssemblyInfo.cs
+++ emerpki/DecenKeep/obj/Debug/net8.0/DecenKeep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecenKeep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6226472385ee07e69c685262786e87c64b8999c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f36254d5209542ee835589401e1bdc2e99ae344")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecenKeep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecenKeep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
