a121b25b93efdd16b34c76073914faf0436333a2
Scott Guyton <kiljoy001@gmail.com>
2024-06-14 18:12:19 -0700

    added changes for custom cscheck generator project

--- emerpki/CustomGenerators/obj/Debug/net8.0/CustomGenerators.AssemblyInfo.fs
+++ emerpki/CustomGenerators/obj/Debug/net8.0/CustomGenerators.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("CustomGenerators")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e8ba05acc1d0b39e036934357db13939d44db4")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f47b1ce231380ef4c8167408cd27564d5d466cd")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("CustomGenerators")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("CustomGenerators")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
